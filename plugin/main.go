161813d9638d779df03c5400b707db0cc4f0503a
Nik Kinkel <nskinkel@iastate.edu>
2016-01-20 12:35:46 -0600

    fix plugin race, handle manager ready reply

--- plugin/main.go
+++ plugin/main.go
@@ -2,49 +2,63 @@ package plugin
 
 import (
     "errors"
+    "log"
     "net"
     "net/http"
     "net/rpc"
     "strconv"
-)
+    "sync"
 
-const (
-    CONTROL_PORT_MIN = 10000
-    CONTROL_PORT_MAX = 10099
-    // TODO: make this importable from projectmain
-    CORE_CONTROL_PORT = "localhost:10100"
+    "github.com/Senior-Design-May1601/projectmain/control"
 )
 
 type Server struct {
     Port int
     listener net.Listener
+    readyChan chan *rpc.Call
 }
 
-func (x *Server) Serve() error {
-    client, err := rpc.DialHTTP("tcp", CORE_CONTROL_PORT)
+func (x *Server) Serve() {
+    client, err := rpc.DialHTTP("tcp", control.CONTROL_PORT_CORE)
     if err != nil {
-        return err
+        log.Fatal(err)
     }
-    // TODO: handle errors here somehow?
-    // TODO: possible race condition?
-    // TODO: handle plugin manager response
-    client.Go("PluginManager.Ready", x.Port, struct{}{}, nil)
-    return http.Serve(x.listener, nil)
+    // TODO: do this better (wait groups feel weird here)
+    var wg sync.WaitGroup
+    wg.Add(1)
+    // Serve needs to run before the client connection to avoid race condition
+    go http.Serve(x.listener, nil)
+    go client.Go("PluginManager.Ready", x.Port, struct{}{}, x.readyChan)
+    wg.Wait()
 }
 
 func NewPluginServer(plugin Plugin) (*Server, error) {
-    // XXX can we handle having multiple "Plugin" names registered?
+    // XXX can we actually handle having multiple "Plugin" names registered?
     rpc.RegisterName("Plugin", plugin)
     rpc.HandleHTTP()
     listener, port, err := getListener()
     if err != nil {
         return nil, err
     }
-    return &Server{port, listener}, nil
+
+    s := &Server{port, listener, make(chan *rpc.Call)}
+
+    // handle exactly one manager ready reply
+    go func() {
+        for call := range s.readyChan {
+            if call.Error != nil {
+                log.Fatal(call.Error)
+            }
+            break
+        }
+        close(s.readyChan)
+    }()
+
+    return s, nil
 }
 
 func getListener() (net.Listener, int, error) {
-    for port := CONTROL_PORT_MIN; port <= CONTROL_PORT_MAX; port++ {
+    for port := control.CONTROL_PORT_MIN; port <= control.CONTROL_PORT_MAX; port++ {
         l, e := net.Listen("tcp", "localhost:" + strconv.Itoa(port))
         if e == nil {
             return l, port, nil
